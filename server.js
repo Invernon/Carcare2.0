2bc7757e83d61f9c555c4db89e37bf56b4ce6a26
Jose Invernon <Jose Invernon>
2016-05-13 17:23:27 -0400

    Actualizacion del Server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(
 	req, res){
 
 	res.writeHead(200, {'Content-Type':'text/html'});
-	res.end('<h1> Hello World </h1>');
+	res.end('<h1> Car Care Para todos - Gracias martin </h1>');
 });
 
 var port = Number(process.env.PORT || 3000 );
