e46e6b97c8caebac1113f7878c96be06b2631beb
Jose Invernon <Jose Invernon>
2016-05-14 17:44:14 -0400

    Prueba3

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-var http = require('http');
 
-var server = http.createServer(function(
-	req, res){
+var express = require('express');
+var app = express();
 
-	res.writeHead(200, {'Content-Type':'text/html'});
-	res.end('<h1> Car Care Para todos </h1>');
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
-var port = Number(process.env.PORT || 3000 );
-
-server.listen(port);
-console.log("Server is listening");
-
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-})
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
