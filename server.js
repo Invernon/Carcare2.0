11d4ed043bb2021e8d6e9390fd7861079c40b885
Jose Invernon <Jose Invernon>
2016-05-15 16:36:27 -0400

    Prueba

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
+// Importando las Configuraciones < CORE MODULES >
+var config 			= require('./config.json');
+var express 		= require('express');
+var serveStatic 	= require('serve-static');
+var pg 				= require('pg');
+//var bodyParser  	= require('body-parser');
+//var multer 			= require('multer');
+//var massive 		= require('massive');
 
-var express = require('express');
+pg.defaults.ssl = true;
+//Conexion a la base de datos:
+
+var connectionString = "postgres://"+config.postgres.user+":"+config.postgres.password+"@"+config.postgres.host+"/"+config.postgres.db;
+//var massiveInstance  = massive.connectSync({connectionString : connectionString});
+var db;
+
+//-------------------------------------//
 var app = express();
 
+var startExpress = function() {
+	app.listen(config.express.port);
+	db = app.get('db');
+}
+
+var initialize 	= function(){
+	startExpress();
+}
+
+console.log("Si corre");
+/*
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8888;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -20,5 +46,31 @@ app.get('/', function(req, res) {
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('listen on: ' + port);
 });
+
+//---------------------------------------------//
+*/
+
+// Send back a 500 error
+var handleError = function(res) {
+    return function(err){
+        console.log(err)
+        res.send(500,{error: err.message});
+    }
+}
+
+//------------------------------------------//
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
